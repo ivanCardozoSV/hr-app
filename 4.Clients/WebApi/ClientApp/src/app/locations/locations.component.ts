582c5bee7e169dc56a40bebd763fb153593ed982
ThomasNazar <ThomasNazar@users.noreply.github.com>
2019-10-01 10:34:34 -0300

    Replaced the collapsed panels in Settings with a side menu and a nested router to display the required components. Modified the spans of many columns in the required components

---
+++ 4.Clients/WebApi/ClientApp/src/app/locations/locations.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { Room } from 'src/entities/room';
+import { Office } from 'src/entities/office';
+import { FacadeService } from '../services/facade.service';
+import { AppComponent } from '../app.component';
+import { ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+  selector: 'app-locations',
+  templateUrl: './locations.component.html',
+  styleUrls: ['./locations.component.css']
+})
+export class LocationsComponent implements OnInit {
+  tab: string;
+
+  emptyRoom: Room[] = [];
+  listOfDisplayDataRoom = [...this.emptyRoom];
+
+  emptyOffice: Office[] = [];
+  listOfDisplayDataOffice = [...this.emptyOffice];
+  
+  constructor(private route: ActivatedRoute, private facade: FacadeService, private app: AppComponent) { }
+
+  ngOnInit() {
+    this.getOffices();
+    this.getRooms();
+    this.tab=this.route.snapshot.params['tab'];
+    this.route.params.subscribe((params: Params)=>{
+      this.tab=this.route.snapshot.params['tab'];
+    });
+  }
+
+  getRooms() {
+    this.facade.RoomService.get<Room>()
+    .subscribe(res => {
+      this.emptyRoom = res;
+      this.listOfDisplayDataRoom = res;
+      }, err => {
+      console.log(err);
+    });
+  }  
+
+  getOffices() {
+    this.facade.OfficeService.get<Office>().subscribe(res => {
+      this.emptyOffice = res;
+      this.listOfDisplayDataOffice = res;
+    }, err => {
+      console.log(err);
+    })
+  }
+
+}
