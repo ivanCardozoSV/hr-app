0e00b3a0dd9ea309fcb60cdadee14493b49adf48
Matias Totaro <matias.totaro@softvision.com>
2019-10-03 11:00:30 -0300

    Create communication with HR API, with authenticationCommand and CandidateCommand

---
+++ 5.Mobile/HrApp/HrApp/API/AuthenticationCommand.cs
@@ -0,0 +1,31 @@
+﻿using HrApp.API.DTO;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace HrApp.API
+{
+    class AuthenticationCommand : HttpCommand
+    {
+        private readonly Credentials credentials; 
+        private readonly string endpoint;
+
+        public AuthenticationCommand (Credentials credentials)
+        {
+            this.credentials = credentials;
+            this.endpoint = api + "Auth/login";
+        }
+
+        public override HttpResponseMessage Execute()
+        {
+            var credentialsJson = JsonConvert.SerializeObject(credentials);
+            var httpClient = HttpReceiver.GetReceiver();
+
+            var res = httpClient.PostJson(endpoint, credentialsJson);
+
+            return res;
+        }
+    }
+}
