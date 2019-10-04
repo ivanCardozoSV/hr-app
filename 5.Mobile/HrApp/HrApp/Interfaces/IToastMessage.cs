74f3428293a9732c1eba3660dfc293aba8d192ce
Matias Totaro <matias.totaro@softvision.com>
2019-10-04 16:42:34 -0300

    Created ToastMessage for Android and IOS

---
+++ 5.Mobile/HrApp/HrApp/Interfaces/IToastMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HrApp.Interfaces
+{
+    public interface IToastMessage
+    {
+        void LongAlert(string message);
+        void ShortAlert(string message);
+    }
+}
