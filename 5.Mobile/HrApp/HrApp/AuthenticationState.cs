c09c671b01528e3b1c76c8ffc8c6276798150087
Tomás Rebollo <tomas.rebollo@softvision.com>
2019-10-04 16:08:29 -0300

    Add google login

---
+++ 5.Mobile/HrApp/HrApp/AuthenticationState.cs
@@ -0,0 +1,9 @@
+﻿using Xamarin.Auth;
+
+namespace HrApp
+{
+    public class AuthenticationState
+    {
+        public static OAuth2Authenticator Authenticator;
+    }
+}
--- 5.Mobile/HrApp/HrApp/Views/LoginView.xaml.cs
+++ 5.Mobile/HrApp/HrApp/Views/LoginView.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace HrApp.Views
@@ -12,6 +6,7 @@ namespace HrApp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LoginView : ContentPage
     {
+
         public LoginView()
         {
             InitializeComponent();
