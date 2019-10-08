60b7e3f8578b19203c4b3bd293dc418e6e47afff
Matias Totaro <matias.totaro@softvision.com>
2019-10-08 18:25:30 -0300

    Add Splash Activity with drawable resources, styles, colors for android

--- 5.Mobile/HrApp/HrApp.Android/MainActivity.cs
+++ 5.Mobile/HrApp/HrApp.Android/MainActivity.cs
@@ -10,7 +10,7 @@ using Plugin.GoogleClient;
 
 namespace HrApp.Droid
 {
-    [Activity(Label = "HrApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "HrApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
---
+++ 5.Mobile/HrApp/HrApp.Android/SplashActivity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace HrApp.Droid
+{
+    [Activity(Label = "HrApp", Theme ="@style/Theme.Splash",MainLauncher =true,
+        NoHistory =true)]
+    public class SplashActivity : AppCompatActivity
+    {
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+        }
+
+        protected override void OnResume()
+        {
+            try
+            {
+                base.OnResume();
+                var intent = new Intent(this, typeof(MainActivity));
+                if (Intent.Extras != null)
+                {
+                    intent.PutExtras(Intent.Extras);
+                }
+                StartActivity(intent);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex);
+            }
+          
+        }
+    }
+}
