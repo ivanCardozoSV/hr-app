937b22aebde8514ecff2b2cf36b9e059d4c3a482
Victor Hidalgo <Victor.Hidalgo@softvision.com>
2019-12-02 16:34:01 -0300

    CandidateViewModel: Model Updated

--- 4.Clients/ApiServer.Contracts/Candidates/UpdateCandidateViewModel.cs
+++ 4.Clients/ApiServer.Contracts/Candidates/UpdateCandidateViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using ApiServer.Contracts.CandidateSkill;
 using ApiServer.Contracts.Community;
 using ApiServer.Contracts.Consultant;
+using ApiServer.Contracts.CandidateProfile;
 using ApiServer.Contracts.Office;
 using Domain.Model.Enum;
 using System;
@@ -24,7 +25,7 @@ namespace ApiServer.Contracts.Candidates
         public ReadedConsultantViewModel Recruiter { get; set; }
         public DateTime ContactDay { get; set; }
         public int PreferredOfficeId { get; set; }
-        public int Profile { get; set; }
+        public ReadedCandidateProfileViewModel Profile { get; set; }
         public ReadedCommunityViewModel Community { get; set; }
         public bool IsReferred { get; set; }
 
